4f8233a3779a5bb9898d74498ddc354ec322d51f
Bennett Moore <bennettmoore05@gmail.com>
2026-01-06 23:02:42 -0500

    chore: update deps and config files

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,5 @@
 import eslintAstroPlugin from 'eslint-plugin-astro';
-import typescriptEslintParser from '@typescript-eslint/parser';
-import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
+import typescriptEslint from 'typescript-eslint';
 import prettierPlugin from 'eslint-plugin-prettier';
 import eslintJs from '@eslint/js';
 import globals from 'globals';
@@ -9,6 +8,7 @@ import type { Linter } from 'eslint';
 const config: Linter.Config[] = [
   eslintJs.configs.recommended,
   ...eslintAstroPlugin.configs.recommended,
+  ...typescriptEslint.configs.recommended,
   {
     ignores: ['.astro/**'],
   },
@@ -21,18 +21,15 @@ const config: Linter.Config[] = [
   {
     files: ['**/*.{js,ts}'],
     languageOptions: {
-      parser: typescriptEslintParser,
       parserOptions: {
         project: './tsconfig.json',
       },
     },
     plugins: {
-      '@typescript-eslint': typescriptEslintPlugin,
       prettier: prettierPlugin,
     },
     rules: {
       'prettier/prettier': ['error'],
-      ...typescriptEslintPlugin.configs.recommended.rules,
     },
   },
 ];
